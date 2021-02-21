80c9fb1ab4d95d343cd358f4fbbe72bf3ec2b3b5
lee-hyunsu <dlgustn675@naver.com>
2021-02-21 15:51:50 +0900

    test

--- chatbot/frontend/src/components/CreateRoomPage.js
+++ chatbot/frontend/src/components/CreateRoomPage.js
@@ -4,6 +4,7 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
 import { Link } from "react-router-dom";
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
@@ -23,6 +24,46 @@ export default class CreateRoomPage extends Component {
                 <Typography component="h4" variant="h4">
                     Create A Room
                 </Typography>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <FormControl component="fieldset">
+                    <FormHelperText>
+                        <div align="center">
+                            Guest Control of Playback State
+                        </div>
+                    </FormHelperText>
+                    <RadioGroup row defaultValue="true">
+                        <FormControlLabel 
+                            value="true" 
+                            control={<Radio color="primary" />}
+                            label="Play/Pause"
+                            labelPlacement="bottom"
+                        />
+                        <FormControlLabel 
+                            value="false" 
+                            control={<Radio color="secondary" />}
+                            label="No Control"
+                            labelPlacement="bottom"
+                        />
+                    </RadioGroup>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <FormControl>
+                    <TextField 
+                        required={true} 
+                        type="number" 
+                        defaultValue={this.defaultVotes}
+                        inputProps={{
+                            min: 1,
+                        }}
+                    />
+                    <FormHelperText>
+                        <div align="center">
+                            Votes Required To Skip Song
+                        </div>
+                    </FormHelperText>
+                </FormControl>
             </Grid>
         </Grid>
         );
