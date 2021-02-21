f71ae371dfe40342b55e3f82e9524f465defbe33
lee-hyunsu <dlgustn675@naver.com>
2021-02-21 15:59:45 +0900

    test

--- chatbot/frontend/src/components/CreateRoomPage.js
+++ chatbot/frontend/src/components/CreateRoomPage.js
@@ -56,6 +56,7 @@ export default class CreateRoomPage extends Component {
                         defaultValue={this.defaultVotes}
                         inputProps={{
                             min: 1,
+                            style: {textAlign: "center"},
                         }}
                     />
                     <FormHelperText>
@@ -64,6 +65,20 @@ export default class CreateRoomPage extends Component {
                         </div>
                     </FormHelperText>
                 </FormControl>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Button color="primary" variant="contained">
+                    Create A Room
+                </Button>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Button 
+                    color="primary" 
+                    variant="contained" 
+                    to="/" 
+                    component={Link}>
+                    Back
+                </Button>
             </Grid>
         </Grid>
         );
