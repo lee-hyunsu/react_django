1be619d5ebf0206beabacec09cf688c5748d9ade
lee-hyunsu <dlgustn675@naver.com>
2021-02-21 15:20:00 +0900

    test

--- chatbot/frontend/src/components/CreateRoomPage.js
+++ chatbot/frontend/src/components/CreateRoomPage.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import { Link } from "react-router-dom";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 export default class CreateRoomPage extends Component {
+    defaultVotes = 2;
+
     constructor(props) {
         super(props);
     }
 
     render() {
-        return <p>This is the chatbot create room page</p>
+        return <Grid container spacing={1}>
+            <Grid item xs={12} align="center"></Grid>
+        </Grid>;
     }
 }
