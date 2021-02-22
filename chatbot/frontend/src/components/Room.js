47e217fe3b505100e707112df886b38fb046d00e
lee-hyunsu <dlgustn675@naver.com>
2021-02-22 11:35:05 +0900

    test

--- chatbot/frontend/src/components/Room.js
+++ chatbot/frontend/src/components/Room.js
@@ -9,6 +9,19 @@ export default class Room extends Component{
             isHost: false,
         };
         this.roomCode = this.props.match.params.roomCode;
+        this.getRoomDetails();
+    }
+
+    getRoomDetails(){
+        fetch('/api/get-room' + '?code=' + this.roomCode).then((response) =>
+            response.json()
+        ).then((data) => {
+            this.setState({
+                votesToSkip: data.votes_to_skip,
+                guestCanPause: data.guest_can_pause,
+                isHost: data.is_host,
+            });
+        });
     }
 
     render(){
@@ -19,10 +32,10 @@ export default class Room extends Component{
                     Votes: {this.state.votesToSkip}
                 </p>
                 <p>
-                    Guest Can Pause: {this.state.guestCanPause}
+                    Guest Can Pause: {this.state.guestCanPause.toString()}
                 </p>
                 <p>
-                    Host: {this.state.isHost}
+                    Host: {this.state.isHost.toString()}
                 </p>
             </div>
         );
