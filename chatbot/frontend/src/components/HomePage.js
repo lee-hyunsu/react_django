4e3c0c2b4777251d767f2fde19ef59cd6e563621
lee-hyunsu <dlgustn675@naver.com>
2021-03-03 23:22:53 +0900

    test

--- chatbot/frontend/src/components/HomePage.js
+++ chatbot/frontend/src/components/HomePage.js
@@ -19,8 +19,21 @@ export default class HomePage extends Component {
     renderHomePage(){
         return(
             <Grid container spacing={3}>
-                <Grid item xs={12} align="center"></Grid>
-                <Grid item xs={12} align="center"></Grid>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h3" compact="h3">
+                        House Party
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <ButtonGroup disableElevation variant="contained" color="primary">
+                        <Button color="primary" to='/join' component={Link}>
+                            Join a Room
+                        </Button>
+                        <Button color="secondary" to='/create' component={Link}>
+                            Create a Room
+                        </Button>
+                    </ButtonGroup>
+                </Grid>
             </Grid>
         );
     }
