d7b63c9a6683b3a3ca529aa99c9e4e8b7ce335b5
lee-hyunsu <dlgustn675@naver.com>
2021-03-06 14:47:16 +0900

    test

--- chatbot/frontend/src/components/Room.js
+++ chatbot/frontend/src/components/Room.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Grid, Button, Typography} from '@material-ui/core'
 
 export default class Room extends Component{
     constructor(props){
@@ -25,19 +26,38 @@ export default class Room extends Component{
     }
 
     render(){
-        return( 
-            <div>
-                <h3>{this.roomCode}</h3>
-                <p>
-                    Votes: {this.state.votesToSkip}
-                </p>
-                <p>
-                    Guest Can Pause: {this.state.guestCanPause.toString()}
-                </p>
-                <p>
-                    Host: {this.state.isHost.toString()}
-                </p>
-            </div>
+        return(
+            <Grid container spacing={1}>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h4" component="h4">
+                        Code: {this.roomCode}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h4" component="h4">
+                        Votes: {this.state.votesToSkip}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h4" component="h4">
+                        Guest Can Pause: {this.state.guestCanPause.toString()}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h4" component="h4">
+                        Host: {this.state.isHost.toString()}
+                    </Typography>
+                </Grid>
+            </Grid>  
         );
     }
 }
+
+/*
+            <div>
+                <h3>{this.roomCode}</h3>
+                <p>Votes: {this.state.votesToSkip}</p>
+                <p>Guest Can Pause: {this.state.guestCanPause.toString()}</p>
+                <p>Host: {this.state.isHost.toString()}</p>
+            </div>
+*/
